e1caa304c666e650b173569ef91578e27bd7de29
antonio <it-an@hotmail.com>
2021-06-15 14:40:07 -0300

    Prepared for deploy in Heroku

--- mytubelibrary/settings.py
+++ mytubelibrary/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
 # ----------------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,16 +123,41 @@ WSGI_APPLICATION = 'mytubelibrary.wsgi.application'
 # }
 
 # --- PostgreSQL --- #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME_DB'),
-        'USER': config('USER_DB'),
-        'PASSWORD': config('PASSWORD_DB'),
-        'HOST': config('HOST_DB'),
-        'PORT': config('PORT_DB'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('NAME_DB'),
+#         'USER': config('USER_DB'),
+#         'PASSWORD': config('PASSWORD_DB'),
+#         'HOST': config('HOST_DB'),
+#         'PORT': config('PORT_DB'),
+#     }
+# }
+
+# --- PostgreSQL in Heroku--- #
+# --- Development --- #
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('NAME_DB'),
+            'USER': config('USER_DB'),
+            'PASSWORD': config('PASSWORD_DB'),
+            'HOST': config('HOST_DB'),
+            'PORT': config('PORT_DB'),
+        }
     }
-}
+
+# --- Prodution --- #
+if not DEBUG:
+    import dj_database_url
+
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
 
 
 # Password validation
@@ -182,8 +208,12 @@ if DEBUG:
 
 # --- Production --- #
 if not DEBUG:
-    STATIC_ROOT = config('STATIC_ROOT')
-    MEDIA_ROOT = config('MEDIA_ROOT')
+    # STATIC_ROOT = config('STATIC_ROOT')
+    # MEDIA_ROOT = config('MEDIA_ROOT')
+
+    # --- For Heroku --- #
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 
 # ----------------------------------------------------------
@@ -194,13 +224,13 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # --- Production --- #
-if not DEBUG:
-    EMAIL_HOST = config('EMAIL_HOST')
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-    EMAIL_PORT = config('EMAIL_PORT', cast=int)
-    EMAIL_USER_SSL = True
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+# if not DEBUG:
+#     EMAIL_HOST = config('EMAIL_HOST')
+#     EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+#     EMAIL_PORT = config('EMAIL_PORT', cast=int)
+#     EMAIL_USER_SSL = True
+#     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+#     DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
 # ----------------------------------------------------------
